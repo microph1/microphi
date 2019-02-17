2d5db7402b6c1e553fe25c966e24dec2375823cd
Davide Cavaliere <cavaliere.davide@gmail.com>
2019-02-17 09:58:29 +0000

    adding mocha reporter

--- client/src/karma.conf.js
+++ client/src/karma.conf.js
@@ -8,8 +8,8 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      // require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-mocha-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
@@ -21,7 +21,7 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'mocha'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,

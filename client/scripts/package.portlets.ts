63a9c40e6a2af6afce1cf1ab5dd46e1781a0c0a7
Davide Cavaliere <cavaliere.davide@gmail.com>
2019-05-24 08:23:52 +0000

    updating postbuild script for hello-portlet

--- client/scripts/package.portlets.ts
+++ client/scripts/package.portlets.ts
@@ -5,15 +5,15 @@ const concat = require('concat');
 (async function build() {
 
   const files = [
-    './dist/hello-portlet/runtime.js',
-    './dist/hello-portlet/es2015-polyfills.js',
+    // './dist/hello-portlet/runtime.js',
+    // './dist/hello-portlet/es2015-polyfills.js',
     './dist/hello-portlet/main.js',
-    './dist/hello-portlet/polyfills.js',
+    // './dist/hello-portlet/polyfills.js',
     // './dist/hello-portlet/styles.js'
     // './dist/hello-portlet/vendor.js'
 
   ];
 
 console.log('creating elements');
-  await concat(files, './dist/hello-portlet/bundle.js');
+  await concat(files, './dist/drugo/assets/hello-portlet.js');
 })();

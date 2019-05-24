eb53753fca75506ba9f6efe29ef49a1b981655c3
Davide Cavaliere <cavaliere.davide@gmail.com>
2019-05-24 08:22:14 +0000

    using shadow dom

--- client/projects/hello-portlet/src/app/app.component.ts
+++ client/projects/hello-portlet/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   // selector: 'hello-portlet',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class AppComponent {
   title = 'hello-portlet';

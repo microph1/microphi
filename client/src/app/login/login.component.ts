4d03d67a6426293d8965468e5d922c2b23baab03
Davide Cavaliere <cavaliere.davide@gmail.com>
2019-05-24 08:48:50 +0000

    fixing validation behaviour

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,11 +1,10 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../services/user/user.service';
 import { AuthService } from '../services/auth/auth.service';
 import { debounceTime, distinctUntilChanged, filter, tap } from 'rxjs/internal/operators';
 import { Router } from '@angular/router';
 import { Log } from '@microgamma/loggator';
-import { $e } from 'codelyzer/angular/styles/chars';
 import { FileService } from '../services/file/file.service';
 
 @Component({
@@ -17,15 +16,17 @@ export class LoginComponent {
   @Log()
   private $log;
 
+  @ViewChild('emailField')
+  private emailField;
 
   public email = new FormControl(null, [Validators.required, Validators.email]);
   public password = new FormControl(null, Validators.required);
-  public file = new FormControl(null, Validators.required);
+  // public file = new FormControl(null, Validators.required);
 
   public user = new FormGroup({
     email: this.email,
     password: this.password,
-    file: this.file
+    // file: this.file
   }, {
     updateOn: 'change'
   });
@@ -33,39 +34,43 @@ export class LoginComponent {
   public authError: boolean = false;
 
   constructor(private userService: UserService, private authService: AuthService, private router: Router, private fileService: FileService) {
+
     this.user.valueChanges.pipe(
       debounceTime(300),
-      filter(_ => {
-        return this.user.valid
-      }),
+      // filter(_ => {
+      //   return this.user.valid
+      // }),
       distinctUntilChanged()
 
     ).subscribe((response) => {
       this.$log.d('values changed', response);
+      this.$log.d('form', this.user);
     });
 
 
-    this.file.valueChanges.pipe(
-      tap((e) => {
-        console.log(e);
-      })
-    ).subscribe(() => {
-      
-    });
+    // this.file.valueChanges.pipe(
+    //   tap((e) => {
+    //     console.log(e);
+    //   })
+    // ).subscribe(() => {
+    //
+    // });
   }
 
   @HostListener('keydown', ['$event.key'])
   public submit(keyLabel?: string) {
 
-    for (let field in this.user.controls) {
-      this.user.controls[field].markAsTouched({onlySelf: true});
-    }
 
     if (keyLabel !== undefined) {
 
       if (keyLabel !== 'Enter') {
         return; // do nothing
       }
+    }
+
+
+    for (let field in this.user.controls) {
+      this.user.controls[field].markAsTouched({onlySelf: true});
     }
 
     this.$log.d('user', this.user.getRawValue());
@@ -77,20 +82,23 @@ export class LoginComponent {
         .subscribe((resp) => {
           this.$log.d('got response', resp);
           this.router.navigateByUrl('');
-        }, () => this.authError = true);
+        }, () => {
+          this.emailField.nativeElement.focus();
+          this.authError = true;
+        });
     }
 
 
   }
 
-  onFileAttachment($event) {
-    console.log($event);
-
-    const file = $event.target.files[0];
-
-    this.fileService.upload(file).subscribe((resp) => {
-      console.log('resp');
-    });
-
-  }
+  // onFileAttachment($event) {
+  //   console.log($event);
+  //
+  //   const file = $event.target.files[0];
+  //
+  //   this.fileService.upload(file).subscribe((resp) => {
+  //     console.log('resp');
+  //   });
+  //
+  // }
 }

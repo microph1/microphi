b37794dd898b8d055bace9c9ce371cc72cfdccf1
Davide Cavaliere <cavaliere.davide@gmail.com>
2019-05-25 08:15:55 +0000

    chore(hello-potlet): setting namespace

--- client/projects/hello-portlet/src/app/app.component.ts
+++ client/projects/hello-portlet/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Log, setNamespace } from '@microgamma/loggator';
+
+setNamespace('hello-portlet');
 
 @Component({
   // selector: 'hello-portlet',

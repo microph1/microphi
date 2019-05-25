19b12ed2ba61959eda3e3d97a8c5c634f416d71e
Davide Cavaliere <cavaliere.davide@gmail.com>
2019-05-25 08:17:30 +0000

    feat(hello-portlet): demostrating parsing objects from portal

--- client/projects/hello-portlet/src/app/app.component.ts
+++ client/projects/hello-portlet/src/app/app.component.ts
@@ -9,11 +9,49 @@ setNamespace('hello-portlet');
   styleUrls: ['./app.component.css'],
   encapsulation: ViewEncapsulation.ShadowDom
 })
-export class AppComponent {
-  title = 'hello-portlet';
+export class AppComponent implements OnInit {
 
-  constructor() {
-    console.log('xtructing AppComponent hello-portlet');
+  @Log()
+  private $log;
+
+
+  // TODO this set, get pattern can we extracted to a decorator
+  private _title;
+
+  @Input()
+  public set title(title) {
+    this._title = JSON.parse(title);
+  };
+
+  public get title() {
+    return this._title;
+  }
+
+  @Input()
+  public set user(value) {
+    this._user = JSON.parse(value);
+  }
+
+  public get user() {
+    return this._user;
+  }
+
+  public _user;
+
+
+  constructor(private changes: ChangeDetectorRef) {
+    this.$log.d('xtructing AppComponent hello-portlet');
     document.dispatchEvent(new Event('portlet:bootstrap'));
+    document.addEventListener('portlet:update',() => {
+      debugger;
+      this.$log.d('got portlet:update');
+      this.$log.d('title is', this.title);
+      this.$log.d('user is', this.user);
+      this.changes.detectChanges();
+    });
+  }
+
+  ngOnInit(): void {
+    document.dispatchEvent(new Event('portlet:ngOnInit'));
   }
 }

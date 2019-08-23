b319cbe893603a3b8ece1063abaf2bdf312bb9d0
Davide Cavaliere <cavaliere.davide@gmail.com>
2019-08-23 12:57:56 +0000

    Revert "adding tickets portlet"
    This reverts commit ac6eebc3e3900c5b1e5bff072ab55dceeae01f3a.

--- portal/apps/portlets/tickets/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'portlets-tickets',
-  preset: '../../../jest.config.js',
-  coverageDirectory: '../../../coverage/apps/portlets/tickets',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};

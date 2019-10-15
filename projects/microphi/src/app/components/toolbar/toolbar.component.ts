26d508326148477b945f4f87d90b008f189619ce
Davide Cavaliere <davide.cavaliere@aegon.nl>
2019-10-15 12:13:34 +0200

    renaming toolbar

--- projects/microphi/src/app/components/toolbar/toolbar.component.ts
+++ projects/microphi/src/app/components/toolbar/toolbar.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { AuthStore } from '../../services/auth/auth.store';
 
 @Component({
-  selector: 'portal-toolbar',
+  selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })

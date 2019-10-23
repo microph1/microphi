7a188698124a2f9eb3b21de220ee7818bf5265bf
Davide Cavaliere <davide.cavaliere@aegon.nl>
2019-10-23 08:49:54 +0200

    adding subscribe pipe

---
+++ projects/microphi/src/app/pipes/subscribe.pipe.ts
@@ -0,0 +1,36 @@
+import { ChangeDetectorRef, Injectable, Pipe, PipeTransform } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthStore } from '../services/auth/auth.store';
+
+@Pipe({
+  name: 'subscribe',
+  pure: true
+})
+export class SubscribePipe implements PipeTransform {
+
+  private subscription;
+
+  public value: any;
+
+  constructor(private changeDetection: ChangeDetectorRef) {
+    console.log('creating instance');
+  }
+
+  transform(observable: Observable<any>, property?: string): any {
+    console.log('subscribing');
+    if (!this.subscription) {
+      this.subscription = observable.subscribe((val) => {
+        console.log('got new data', val);
+        this.changeDetection.detectChanges();
+        this.value = val;
+      });
+    }
+
+    if (property) {
+      return this.value[property];
+    } else {
+      return this.value;
+    }
+  }
+
+}

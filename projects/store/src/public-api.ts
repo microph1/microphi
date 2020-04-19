94f394bf514516f9d4844c1d9df6f6c8df9d3ffd
Davide Cavaliere <dcavaliere@datakitchen.io>
2020-04-19 12:10:27 +0200

    chore(store): move observable-list into its own folder

--- projects/store/src/public-api.ts
+++ projects/store/src/public-api.ts
@@ -8,7 +8,7 @@ export * from './lib/base-store';
 export * from './lib/effect';
 export * from './lib/reduce';
 export * from './lib/store';
-export * from './lib/observable-list';
+export * from './lib/observable-list/observable-list';
 
 export * from './lib/http-status/http-status.interceptor';
 export * from './lib/http-status/http-status.store';

7e7843cddcf7122c316fbbacbc22b80a9d27b5f1
Davide Cavaliere <dcavaliere@datakitchen.io>
2023-01-15 11:25:24 +0100

    chore: add tailwind css to web-component
    and scaffold metronoman-flux project

---
+++ apps/web-components/src/components/fx-metronome.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input } from '@flux/core';
+
+@Component({
+  selector: 'fx-bpm',
+  template: `
+    <div>
+
+      <h2>BPM: {{bpm}}</h2>
+      <button (click)="play()">play</button>
+    </div>
+  `
+})
+export class FxMetronomeComponent {
+  bpm: number = 60;
+
+  private _audioCtx: AudioContext = new AudioContext();
+  private currentBeat: number;
+
+  play() {
+    console.log('start at', this.bpm);
+
+
+    this.beat();
+  }
+
+
+  private beat() {
+    const now = this._audioCtx.currentTime;
+    const osc = this._audioCtx.createOscillator();
+    osc.type = 'triangle';
+    if (this.currentBeat === 0) {
+      osc.frequency.value = 864;
+    } else {
+      osc.frequency.value = 432;
+    }
+
+    const gainNode = this._audioCtx.createGain();
+    osc.connect(gainNode);
+    gainNode.connect(this._audioCtx.destination);
+
+    osc.start(now);
+    gainNode.gain.linearRampToValueAtTime(0, now + 0.06);
+    osc.stop(now + 0.1);
+
+    this.currentBeat = (this.currentBeat + 1) % this.bpm;
+  }
+
+
+}

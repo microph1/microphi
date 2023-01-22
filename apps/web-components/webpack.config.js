9a5b11e83d2e92e3e81ad7abfccef2f58aa43863
Davide Cavaliere <dcavaliere@datakitchen.io>
2023-01-22 17:38:43 +0100

    chore: add deps and remove unnecessary import

--- apps/web-components/webpack.config.js
+++ apps/web-components/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const {TailwindCSSWebpackPlugin} = require("tailwindcss-webpack-plugin");
 
 module.exports = {
   mode: 'development',

fda972251d32dcf055837afb04d318dc76a72610
Davide Cavaliere <cavaliere.davide@gmail.com>
2024-05-14 16:03:58 +0200

    fix(di): in cjs exporting testing modules causes troubles

--- packages/di/src/index.ts
+++ packages/di/src/index.ts
@@ -2,8 +2,6 @@ export * from './lib/injctable/injectable.decorator';
 export * from './lib/aspects/before.decorator';
 export * from './lib/aspects/after.decorator';
 export * from './lib/container/di.decorator';
-export * from './lib/testing/mocked';
-export * from './lib/testing/test-bed';
 
 export * from './lib/provider/providers';
 export * from './lib/injctable/injector';

0065ac6b51893951a1ff5b835713b2ac9e73f6ea
Davide Cavaliere <cavaliere.davide@gmail.com>
2024-11-03 10:05:02 +0100

    fix(store): add new stuff to public API

--- packages/store/src/index.ts
+++ packages/store/src/index.ts
@@ -5,3 +5,6 @@
 export * from './lib/store/store';
 export * from './lib/effect/effect';
 export * from './lib/reduce/reduce';
+export * from './lib/operators/cache';
+export * from './lib/operators/debounce';
+export * from './lib/store/types';

7158801d59a329b0860edf5d844682280ec4707b
Davide Cavaliere <cavaliere.davide@gmail.com>
2024-11-08 16:37:22 +0100

    chore(erratic-lemon): remove unnecessary files and deps

--- apps/erratic-lemon/src/ui/flux-app.ts
+++ apps/erratic-lemon/src/ui/flux-app.ts
@@ -1,6 +1,4 @@
 import { App } from '@microphi/flux';
-import mdcAutoInit from '@material/auto-init';
-import { MDCRipple } from '@material/ripple';
 import { FxFor, FxIf, FxRootComponent, TestComponentSh } from './components/fx-root/fx-root.component';
 
 
@@ -23,16 +21,5 @@ export class FluxApp {
   constructor() {
 
     console.log('app started');
-
-
-    mdcAutoInit(document);
-
-    document.querySelectorAll('.mdc-button').forEach((elm) => {
-      new MDCRipple(elm);
-    });
-
-    document.querySelectorAll('.mdc-icon-button').forEach((elm) => {
-      new MDCRipple(elm).unbounded = true;
-    })
   }
 }

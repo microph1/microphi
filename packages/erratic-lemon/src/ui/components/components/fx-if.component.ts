ebdfc91647446b5228de0a5ebd4d8000665ad291
Davide Cavaliere <cavaliere.davide@gmail.com>
2024-11-15 09:25:53 +0100

    feat(erratic-lemon): iteration update website
    - make package private
    - add fx-if example
    - move content inside fx-root
    - general styles update

--- packages/erratic-lemon/src/ui/components/components/fx-if.component.ts
+++ packages/erratic-lemon/src/ui/components/components/fx-if.component.ts
@@ -3,10 +3,26 @@ import { Input } from '@microphi/flux';
 
 @Component({
   selector: 'fx-if',
-  template: `
-    <slot style="display: {{condition}}"></slot>
-  `
+  template: '<!-- empty: will add content when needed -->',
 })
 export class FxIf {
   @Input() condition!: boolean;
+
+  private slot!: HTMLSlotElement;
+  private nodeElm!: Node;
+
+  constructor(private elm: HTMLElement) {
+    this.slot = document.createElement('slot');
+  }
+
+
+  fxOnChanges() {
+
+    if (this.condition) {
+      this.nodeElm = this.elm.appendChild(this.slot);
+    } else {
+      this.elm.shadowRoot!.removeChild(this.nodeElm);
+    }
+
+  }
 }
